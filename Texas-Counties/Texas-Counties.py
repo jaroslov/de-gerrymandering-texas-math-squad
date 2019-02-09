89a61641a68702643d6c979d043cc92f940d5533
Jacob Smith <jnsmith@apple.com>
2019-02-09 11:05:14 -0600

    [txcounty] Fancy new map generator.

--- Texas-Counties/Texas-Counties.py
+++ Texas-Counties/Texas-Counties.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 import re
 import sys
 import xml.etree.ElementTree as ET
 
 if __name__=="__main__":
-    TXML    = ET.parse("Texas-Counties.svg")
-    SVG     = TXML.getroot()
-    JSON    = json.loads(open("Texas-Counties.json").read())
-    Style   = SVG.findall('.//{http://www.w3.org/2000/svg}style')[0]
-    years   = [info['county-wide-polling'] for key,info in JSON.items() if 'county-wide-polling' in info.keys()]
-    minY    = min(years)
-    maxY    = max(years)
-    for key,info in JSON.items():
-        if 'county-wide-polling' in info.keys():
-            baseColor   = (0x20, 0x63, 0x9C)
-            nextColor   = (0x80, 0xC3, 0xFC)
-            yearInter   = (info['county-wide-polling'] - minY) / float(maxY - minY) # derp if maxY == minY
-            colrInter   = tuple([int((nextC - baseC) * yearInter + baseC) for baseC,nextC in zip(baseColor, nextColor)])
-            colrInter   = nextColor
-            path_id     = 'TX_%s'%(key.replace(' ', '_'))
-            Style.text  += '\n'
-            Style.text  += '        #'+path_id+'{ fill:#%02X%02X%02X;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'%colrInter
-            Style.text  += '        #'+path_id+':hover { fill:#0061B6;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'
-    TXML.write('Texas-Counties-Mod.svg')
+    parser  = argparse.ArgumentParser(description='Build maps of Texas with facts')
+    parser.add_argument('-o', '--output', help="The name of the output file", required=True)
+    parser.add_argument('-i', '--info', help="Which set of facts to export", required=True)
+    parser.add_argument('-t', '--title', help="Optional title")
+
+    args        = parser.parse_args()
+
+    TXML        = ET.parse("Texas-Counties.svg")
+    SVG         = TXML.getroot()
+    JSON        = json.loads(open("Texas-Counties.json").read())
+    Style       = SVG.findall('.//{http://www.w3.org/2000/svg}style')[0]
+    years       = [info['county-wide-polling'] for key,info in JSON.items() if 'county-wide-polling' in info.keys()]
+    minY        = min(years)
+    maxY        = max(years)
+    mapTitle    = args.title if args.title is not None else ' '.join([X.capitalize() for X in args.info.replace('-', ' ').replace('_', ' ').split(' ')])
+    mTElt       = SVG.findall('.//{http://www.w3.org/2000/svg}text[@id="map_title"]')[0]
+    mTElt.text  = mapTitle
+    for county,info in JSON.items():
+        if args.info in info.keys():
+            path_id         = 'TX_%s'%(county.replace(' ', '_'))
+            Style.text      += '        #'+path_id+'{ fill:#%02X%02X%02X;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'%(0x60, 0xB3, 0xDC)
+            Style.text      += '        #'+path_id+':hover { fill:#0061B6;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'
+            itsText         = SVG.findall('.//{http://www.w3.org/2000/svg}text[@id="text_%s"]'%path_id)[0]
+            itsText.text    = county + ' ' + str(info[args.info])
+    TXML.write(args.output)
