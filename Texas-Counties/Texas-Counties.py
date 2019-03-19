8f3a2a0e6a5b432f37ef4a4ee3ffb83f8a1c4a00
Jacob Smith <jnsmith@apple.com>
2019-03-19 08:02:56 -0500

    [dgtms] Contact map.

--- Texas-Counties/Texas-Counties.py
+++ Texas-Counties/Texas-Counties.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import re
 import sys
+import textwrap
 import xml.etree.ElementTree as ET
 
 if __name__=="__main__":
@@ -14,6 +15,8 @@ if __name__=="__main__":
 
     args        = parser.parse_args()
 
+    ET.register_namespace('svg', 'http://www.w3.org/2000/svg')
+    ET.register_namespace('xlink', "http://www.w3.org/1999/xlink")
     TXML        = ET.parse("Texas-Counties.svg")
     SVG         = TXML.getroot()
     JSON        = json.loads(open("Texas-Counties.json").read())
@@ -27,8 +30,37 @@ if __name__=="__main__":
     for county,info in JSON.items():
         if args.info in info.keys():
             path_id         = 'TX_%s'%(county.replace(' ', '_'))
-            Style.text      += '        #'+path_id+'{ fill:#%02X%02X%02X;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'%(0x60, 0xB3, 0xDC)
-            Style.text      += '        #'+path_id+':hover { fill:#0061B6;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'
-            itsText         = SVG.findall('.//{http://www.w3.org/2000/svg}text[@id="text_%s"]'%path_id)[0]
-            itsText.text    = county + ' ' + str(info[args.info])
+            #Style.text      += '        #'+path_id+'{ fill:#%02X%02X%02X;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'%(0x60, 0xB3, 0xDC)
+            #Style.text      += '        #'+path_id+':hover { fill:#0061B6;fill-opacity:1;stroke:#000000;stroke-width:0.3; }\n'
+            itsText         = SVG.findall('.//{http://www.w3.org/2000/svg}g[@id="fact_%s"]'%path_id)[0]
+            theText         = ET.Element("{http://www.w3.org/2000/svg}text")
+            xlink           = ET.Element("{http://www.w3.org/2000/svg}a")
+            theText.set('x', '600')
+            theText.set('y', '384')
+            xlink.set('xlink:href', info['url'])
+            xlink.text      = county
+            xlink.set('text-decoration', 'underline')
+            theText.append(xlink)
+            itsText.append(theText)
+
+            paragraph       = info['contact'].split('\n')
+            instrs          = info['instructions'].replace('\n', ' ')
+            instrs          = textwrap.wrap(instrs, width=36)
+            paragraph       += ['']
+            paragraph       += instrs
+
+
+            for II,subcontact in enumerate(paragraph):
+                theContact      = ET.Element("{http://www.w3.org/2000/svg}text")
+                theContact.text = subcontact
+                theContact.set('x', '600')
+                theContact.set('y', '%s'%(400+10*II))
+                itsText.append(theContact)
+            #itsText.text    = county + ' ' + str(info[args.info])
     TXML.write(args.output)
+
+    FEH     = open(args.output).read()
+    with open(args.output, 'w') as feh:
+        FEH = FEH.replace('xmlns:svg="http://www.w3.org/2000/svg"', 'xmlns:svg="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink"')
+        print >> feh, FEH
+
---
+++ Texas-Counties/cbc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import csv
+import json
+
+oldTCJ  = json.loads(open('Texas-Counties.json').read())
+
+with open('cbc.csv', 'rb') as csvfile:
+    rd = csv.reader(csvfile)
+    for row in rd:
+        theCounty   = ' '.join(row[0].split(' ')[:-1])
+        newData = '%(link)s"'%{
+                            'link'      : row[1],
+                        }
+        oldTCJ[theCounty]['url']            = newData
+        oldTCJ[theCounty]['contact']        = row[2]
+        oldTCJ[theCounty]['instructions']   = row[3]
+
+with open('Texas-Counties2.json', 'w') as tcj:
+    print >> tcj, json.dumps(oldTCJ, indent=4, sort_keys=True)
